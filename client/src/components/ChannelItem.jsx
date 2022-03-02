fa7b8b5626bf32678032ed2d0aeb0712d560a9c9
MarioWork <mariovieira.work@hotmail.com>
2022-03-02 21:14:29 -0100

    Only can change channel members if is the owner

--- client/src/components/ChannelItem.jsx
+++ client/src/components/ChannelItem.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { StyledChannelItem } from "./styles/ChannelItem.styled";
+import Cookies from "universal-cookie";
 
 const ChannelItem = ({ channel, onClick, setShowMemberList }) => {
+  const [isOwner, setIsOwner] = useState(false);
+  const cookies = new Cookies();
+
+  useEffect(() => {
+    setIsOwner(channel.data.created_by.id === cookies.get("userId"));
+  }, []);
+
   function addMemberClick(e) {
     e.preventDefault();
     e.stopPropagation();
@@ -17,7 +25,7 @@ const ChannelItem = ({ channel, onClick, setShowMemberList }) => {
         <h2>{channel.data.name.charAt(0).toUpperCase()}</h2>
       )}
       <p>{channel.data.name}</p>
-      <FaUser onClick={addMemberClick} color="blue" />
+      {isOwner && <FaUser onClick={addMemberClick} color="blue" />}
     </StyledChannelItem>
   );
 };
